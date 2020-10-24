e78c3b3b02c2db5aa86e03bf42c1493b0c692ffe
Wojciech Szyjka <wojtekszyjka@outlook.com>
2020-10-24 11:25:39 +0200

    Add project files.

---
+++ Zadanie2-plik3-pdf/Zadanie2-plik3-pdf.cpp
@@ -0,0 +1,34 @@
+﻿// Wojtek Szyjka 2020 //
+
+
+#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+const float Pi = 3.1415;
+float r;
+float ok;
+float pk;
+float pc;
+float obk;
+float ulg; 
+float uld;
+int main() {
+	cout << "Witaj, podaj promien : "; 
+	cin >> r;
+	ok = 2 * Pi * r;
+	pk = Pi * r;
+	pc = (r * r) * 4 * Pi ;
+	ulg = 4;
+	uld = 3;
+	obk = (ulg / uld) * Pi * (r * r * r);
+
+	cout << "Obwód kola = "<< ok << endl;
+	cout << "Pole kola = " << pk << endl;
+	cout << "Powierzchnia calkowita kuli = " <<  pc << endl;
+	cout << "Objetosc kuli = " << obk << endl;
+	return (0);
+}
